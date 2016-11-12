d527230edb1f08f021a48d044475f70bfbc34709
Mohammad Jedari <mtjedari@Mohammads-Mac.local>
2016-11-12 09:04:26 -0800

    12-11

--- nfbox/nfbox/NFBProfileViewController.swift
+++ nfbox/nfbox/NFBProfileViewController.swift
@@ -25,52 +25,53 @@ class NFBProfileViewController: UIViewController {
     @IBOutlet weak var viewBMI: UIView!
     @IBOutlet weak var progress: UIImageView!
 
-    @IBOutlet weak var liveStreamBtn: UIButton!
+    @IBOutlet weak var liveStreamBtn: UIImageView!
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        viewProfile.layer.shadowColor = UIColor.black.cgColor
-        viewProfile.layer.shadowOpacity = 0.2
-        viewProfile.layer.shadowOffset = CGSize(width: 1, height: 1)
-        viewProfile.layer.shadowRadius = 1
-        viewProfile.layer.shadowPath = UIBezierPath(rect: viewProfile.bounds).cgPath
-        ////////////////////
-        viewGained1.layer.shadowColor = UIColor.black.cgColor
-        viewGained1.layer.shadowOpacity = 0.2
-        viewGained1.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        viewGained1.layer.shadowRadius = 4
-        viewGained1.layer.shadowPath = UIBezierPath(rect: viewGained1.bounds).cgPath
-        ////////////////////
-        viewGained2.layer.shadowColor = UIColor.black.cgColor
-        viewGained2.layer.shadowOpacity = 0.2
-        viewGained2.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        viewGained2.layer.shadowRadius = 4
-        viewGained2.layer.shadowPath = UIBezierPath(rect: viewGained2.bounds).cgPath
-        ////////////////////
-        viewWeight.layer.shadowColor = UIColor.black.cgColor
-        viewWeight.layer.shadowOpacity = 0.2
-        viewWeight.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        viewWeight.layer.shadowRadius = 4
-        viewWeight.layer.shadowPath = UIBezierPath(rect: viewWeight.bounds).cgPath
-        ////////////////////
-        viewGoal.layer.shadowColor = UIColor.black.cgColor
-        viewGoal.layer.shadowOpacity = 0.2
-        viewGoal.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        viewGoal.layer.shadowRadius = 4
-        viewGoal.layer.shadowPath = UIBezierPath(rect: viewWeight.bounds).cgPath
-        ////////////////////
-        viewBMI.layer.shadowColor = UIColor.black.cgColor
-        viewBMI.layer.shadowOpacity = 0.2
-        viewBMI.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        viewBMI.layer.shadowRadius = 4
-        viewBMI.layer.shadowPath = UIBezierPath(rect: viewBMI.bounds).cgPath
-        ////////////////////
-        progress.layer.shadowColor = UIColor.black.cgColor
-        progress.layer.shadowOpacity = 0.2
-        progress.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        progress.layer.shadowRadius = 3
-        progress.layer.shadowPath = UIBezierPath(rect: progress.bounds).cgPath
+//        viewProfile.layer.shadowColor = UIColor.black.cgColor
+//        viewProfile.layer.shadowOpacity = 0.2
+//        viewProfile.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        viewProfile.layer.shadowRadius = 1
+//        viewProfile.layer.shadowPath = UIBezierPath(rect: viewProfile.bounds).cgPath
+//        ////////////////////
+//        viewGained1.layer.shadowColor = UIColor.black.cgColor
+//        viewGained1.layer.shadowOpacity = 0.2
+//        viewGained1.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+//        viewGained1.layer.shadowRadius = 4
+//        viewGained1.layer.shadowPath = UIBezierPath(rect: viewGained1.bounds).cgPath
+//        ////////////////////
+//        viewGained2.layer.shadowColor = UIColor.black.cgColor
+//        viewGained2.layer.shadowOpacity = 0.2
+//        viewGained2.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+//        viewGained2.layer.shadowRadius = 4
+//        viewGained2.layer.shadowPath = UIBezierPath(rect: viewGained2.bounds).cgPath
+//        ////////////////////
+//        viewWeight.layer.shadowColor = UIColor.black.cgColor
+//        viewWeight.layer.shadowOpacity = 0.2
+//        viewWeight.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+//        viewWeight.layer.shadowRadius = 4
+//        viewWeight.layer.shadowPath = UIBezierPath(rect: viewWeight.bounds).cgPath
+//        ////////////////////
+//        viewGoal.layer.shadowColor = UIColor.black.cgColor
+//        viewGoal.layer.shadowOpacity = 0.2
+//        viewGoal.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+//        viewGoal.layer.shadowRadius = 4
+//        viewGoal.layer.shadowPath = UIBezierPath(rect: viewWeight.bounds).cgPath
+//        ////////////////////
+//        viewBMI.layer.shadowColor = UIColor.black.cgColor
+//        viewBMI.layer.shadowOpacity = 0.2
+//        viewBMI.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+//        viewBMI.layer.shadowRadius = 4
+//        viewBMI.layer.shadowPath = UIBezierPath(rect: viewBMI.bounds).cgPath
+//        ////////////////////
+//        progress.layer.shadowColor = UIColor.black.cgColor
+//        progress.layer.shadowOpacity = 0.2
+//        progress.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+//        progress.layer.shadowRadius = 3
+//        progress.layer.shadowPath = UIBezierPath(rect: progress.bounds).cgPath
         self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
     }
     
