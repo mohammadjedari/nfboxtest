ec154577424a74859d4346d677c8d41e08c05f26
= <=>
2016-11-26 08:46:55 -0500

    26-11

--- nfbox/nfbox/Tools.swift
+++ nfbox/nfbox/Tools.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class Tools: UIViewController {
+class Tools: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    let pickerData = ["kg", "lbs"]
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,18 +24,26 @@ class Tools: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myLabel.text = pickerData[row]
+    }
+    
 
+    
     @IBAction func dismissTools(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
