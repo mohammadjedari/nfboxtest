1e3905c9465dfb7657d4747506d27b0c6632c081
= <=>
2016-12-04 11:36:07 -0500

    4-12

--- nfbox/nfbox/ContactUsViewController.swift
+++ nfbox/nfbox/ContactUsViewController.swift
@@ -10,21 +10,68 @@ import UIKit
 
 class ContactUsViewController: ViewController {
 
-
-    
- 
     @IBOutlet weak var FeedBack: UIView!
     
     @IBOutlet weak var ReportBug: UIView!
  
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
+    @IBOutlet weak var fbView: UIView!
+   
+    @IBOutlet weak var twitterView: UIView!
+    
+    @IBOutlet weak var instaView: UIView!
+    
+    @IBOutlet weak var youtubeView: UIView!
+    
+    @IBOutlet weak var feedbackTextView: UITextView!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        FeedBack.isHidden = false
-        ReportBug.isHidden = true
+//          FeedBack.isHidden = false
+//          ReportBug.isHidden = true
+//        
+//        fbView.layer.shadowColor = UIColor.black.cgColor
+//        fbView.layer.shadowOpacity = 0.2
+//        fbView.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        fbView.layer.shadowRadius = 4
+//        fbView.layer.shadowPath = UIBezierPath(rect: fbView.bounds).cgPath
+//        
+//        twitterView.layer.shadowColor = UIColor.black.cgColor
+//        twitterView.layer.shadowOpacity = 0.2
+//        twitterView.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        twitterView.layer.shadowRadius = 4
+//        twitterView.layer.shadowPath = UIBezierPath(rect: twitterView.bounds).cgPath
+//        
+//        instaView.layer.shadowColor = UIColor.black.cgColor
+//        instaView.layer.shadowOpacity = 0.2
+//        instaView.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        instaView.layer.shadowRadius = 4
+//        instaView.layer.shadowPath = UIBezierPath(rect: instaView.bounds).cgPath
+//        
+//        youtubeView.layer.shadowColor = UIColor.black.cgColor
+//        youtubeView.layer.shadowOpacity = 0.2
+//        youtubeView.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        youtubeView.layer.shadowRadius = 4
+//        youtubeView.layer.shadowPath = UIBezierPath(rect: youtubeView.bounds).cgPath
+//        
+//        feedbackTextView.layer.shadowColor = UIColor.black.cgColor
+//        feedbackTextView.layer.shadowOpacity = 0.2
+//        feedbackTextView.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        feedbackTextView.layer.shadowRadius = 4
+//        feedbackTextView.layer.shadowPath = UIBezierPath(rect: feedbackTextView.bounds).cgPath
+//        
+//        descriptionTextView.layer.shadowColor = UIColor.black.cgColor
+//        descriptionTextView.layer.shadowOpacity = 0.2
+//        descriptionTextView.layer.shadowOffset = CGSize(width: 1, height: 1)
+//        descriptionTextView.layer.shadowRadius = 4
+//        descriptionTextView.layer.shadowPath = UIBezierPath(rect: descriptionTextView.bounds).cgPath
     }
    
+    
     @IBAction func indexChanged(_ sender: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex
         {
@@ -47,5 +94,4 @@ class ContactUsViewController: ViewController {
     @IBAction func dismissContactUs(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
-
 }
--- nfbox/nfbox/RegisterScreen.swift
+++ nfbox/nfbox/RegisterScreen.swift
@@ -11,34 +11,51 @@ import UIKit
 class RegisterScreen: UIViewController {
     
 
-    @IBOutlet var account: UIView!
-    @IBOutlet var personal: UIView!
-    @IBOutlet var subscription: UIView!
-    @IBOutlet weak var back: UILabel!
-    
+    @IBOutlet weak var account: UIView!
+    @IBOutlet weak var personal: UIView!
+    @IBOutlet weak var subscription: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        self.imageView.layer.cornerRadius = self.imageView.frame.size.width/2
     }
     
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            account.isHidden = false
+            personal.isHidden = true
+            subscription.isHidden = true
+        case 1:
+            account.isHidden = true
+            personal.isHidden = false
+            subscription.isHidden = true
+        case 2:
+            account.isHidden = true
+            personal.isHidden = true
+            subscription.isHidden = false
+        default:
+            break;
+        }
+    }
+    
+    @IBAction func dismissRegister(_ sender: UIButton) {
+         self.dismiss(animated: true, completion: nil)
+    }
+   
     
     
-    @IBAction func accountBtn(_ sender: UIButton) {
-        
-    }
-    @IBAction func personalBtn(_ sender: UIButton) {
-        print(10)
-        UIView.animate(withDuration: 0.5, animations: {
-            self.account.alpha = 0.0
-            self.personal.alpha = 1.0
-            self.subscription.alpha = 0.0
-        })
-    }
-    @IBAction func subscribtionBtn(_ sender: UIButton) {
-        
-    }
-    
+   
+//    @IBAction func personalBtn(_ sender: UIButton) {
+//        print(10)
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.account.alpha = 0.0
+//            self.personal.alpha = 1.0
+//            self.subscription.alpha = 0.0
+//        })
+//    }
+   
     
    
     
