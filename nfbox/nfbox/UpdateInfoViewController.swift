c1d1fcda38d24c5b91e854e87c159198ae2fdb7b
= <=>
2016-12-08 12:09:02 -0500

    8-12

--- nfbox/nfbox/UpdateInfoViewController.swift
+++ nfbox/nfbox/UpdateInfoViewController.swift
@@ -18,12 +18,57 @@ class UpdateInfoViewController: UIViewController {
         vc.transitioningDelegate = exampleTransitionDelegate
         present(vc, animated: true, completion: nil)
     }
+    @IBAction func infoButton(_ sender: AnyObject) {
+        var usernameTextField: UITextField?
+        var passwordTextField: UITextField?
+        
+    
+        let alertController = UIAlertController(
+            title: "Log in",
+            message: "Please enter your credentials",
+            preferredStyle: UIAlertControllerStyle.alert)
+        
+    
+        let loginAction = UIAlertAction(
+        title: "Log in", style: UIAlertActionStyle.default) {
+            (action) -> Void in
+            
+            if let username = usernameTextField?.text {
+                print(" Username = \(username)")
+            } else {
+                print("No Username entered")
+            }
+            
+            if let password = passwordTextField?.text {
+                print("Password = \(password)")
+            } else {
+                print("No password entered")
+            }
+        }
+        
+        
+        alertController.addTextField {
+            (txtUsername) -> Void in
+            usernameTextField = txtUsername
+            usernameTextField!.placeholder = "Your username here"
+        }
+        
+        alertController.addTextField {
+            (txtPassword) -> Void in
+            passwordTextField = txtPassword
+            passwordTextField!.isSecureTextEntry = true
+            passwordTextField!.placeholder = "Your password here"
+        }
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
